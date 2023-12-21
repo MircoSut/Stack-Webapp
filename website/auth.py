1e31e132e5214289e86d2d3971a27f98878ff659
MircoSut <mircosut76@gmail.com>
2023-12-21 10:35:28 +0100

    Added Nabvar and Footer

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@auth.route("/logout", methods=["GET", "POST"])
+def logout():
+    return render_template("logout.html")
+
+@auth.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign_up.html")
